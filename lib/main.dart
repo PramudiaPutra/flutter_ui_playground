516a2bc8725af27ee99d8c58890eea0326117c66
Pramudia_Putra_Pamungkas <pramudiaputra.iv@gmail.com>
2021-12-26 22:59:23 +0700

    create scroll view

--- lib/main.dart
+++ lib/main.dart
@@ -1,101 +1,196 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+// import 'package:minyak_jelantah/ui/home/poin/poin2_page.dart';
 
+// import '../../constants.dart';
+
+//debug
 void main() {
-  runApp(const MaterialApp(home: MyApp()));
+  runApp(const MaterialApp(home: PoinPage3()));
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
+class PoinPage3 extends StatefulWidget {
+  const PoinPage3({Key? key}) : super(key: key);
 
   @override
-  _MyAppState createState() => _MyAppState();
+  _PoinPage3State createState() => _PoinPage3State();
 }
 
-class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
-  static const Color recarbonPrimary = Color(0xff06747B);
-  static const recarbonSecondary = Color(0xff025D63);
-
-  TabBar tab = TabBar(
-      indicatorPadding: const EdgeInsets.all(8),
-      overlayColor: null,
-      unselectedLabelColor: Colors.white,
-      labelColor: recarbonSecondary,
-      isScrollable: true,
-      tabs: <Tab>[
-        Tab(
-          child: Row(
-            children: const [
-              Icon(Icons.camera_enhance_sharp),
-              Text('Stories'),
-            ],
-          ),
-        ),
-        Tab(
-          child: Row(
-            children: const [
-              Icon(Icons.location_on),
-              Text('Location'),
-            ],
-          ),
-        ),
-        Tab(
-          child: Row(
-            children: const [
-              Icon(Icons.account_balance),
-              Text('Community'),
-            ],
-          ),
-        ),
-      ],
-      indicator: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(50.0),
-      ));
-
+class _PoinPage3State extends State<PoinPage3> {
   @override
   Widget build(BuildContext context) {
-    double screenHeight = MediaQuery.of(context).size.height;
-
-    return MaterialApp(
-      theme: ThemeData(
-          splashColor: Colors.transparent, highlightColor: Colors.transparent),
-      home: DefaultTabController(
-        length: 3,
-        child: Scaffold(
-          appBar: PreferredSize(
-            preferredSize: Size.fromHeight(screenHeight * 0.12),
-            child: Container(
-              decoration: const BoxDecoration(
-                  color: recarbonPrimary,
-                  borderRadius:
-                      BorderRadius.vertical(bottom: Radius.circular(20))),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Flexible(
-                    child: SizedBox(
-                      height: screenHeight * 0.054,
+    var height = MediaQuery.of(context).size.height;
+    var width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: const Color(0xFFEF9D10),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: Color(0xFF314A08)),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+      ),
+      body: Column(
+        children: [
+          Stack(
+            children: [
+              //ganti gambar produk
+              Image.asset("assets/images/bgPoin.jpeg"),
+              ClipPath(
+                clipper: CurvedHeader(),
+                child: Container(
+                  height: 110,
+                  decoration: BoxDecoration(
+                    color: const Color(0xFFEF9D10),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 5,
+                        blurRadius: 7,
+                        offset:
+                            const Offset(0, 3), // changes position of shadow
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Container(
+            height: 100,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  spreadRadius: 5,
+                  blurRadius: 7,
+                  offset: const Offset(0, 3), // changes position of shadow
+                ),
+              ],
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                //kiri
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(18.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        Text(
+                          'POIN',
+                          style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.black38,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          '500',
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.orange,
+                          ),
+                        )
+                      ],
                     ),
                   ),
-                  Card(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(50.0),
-                    ),
-                    color: recarbonSecondary,
-                    child: tab,
+                ),
+                const VerticalDivider(
+                  color: Colors.black,
+                ),
+                //kanan
+                Expanded(
+                    child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'Berlaku hingga',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.black38,
+                        ),
+                      ),
+                      const SizedBox(height: 8),
+                      Text(
+                        '12 Januari 2021',
+                        style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.black.withOpacity(0.7),
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ],
                   ),
-                  Flexible(
-                    child: SizedBox(
-                      height: screenHeight * 0.054,
+                ))
+              ],
+            ),
+          ),
+          Container(height: 18, color: Colors.grey.withOpacity(0.2)),
+          Expanded(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(18.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Syarat dan Ketentuan',
+                      style: TextStyle(
+                        color: Colors.black.withOpacity(0.7),
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                  ),
-                ],
+                    const SizedBox(height: 18),
+                    const Text(
+                      '"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur? "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?""',
+                      style: TextStyle(color: Colors.grey),
+                    )
+                  ],
+                ),
               ),
             ),
           ),
-        ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0, 12, 0, 18),
+            child: SizedBox(
+              width: width / 1.4,
+              height: 45,
+              child: FloatingActionButton.extended(
+                backgroundColor: const Color(0xFFEF9D10),
+                onPressed: () {},
+                label: const Text(
+                  "Tukar Poin",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+          )
+        ],
       ),
     );
+  }
+}
+
+class CurvedHeader extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+    final lowPoint = size.height - 30;
+    final highPoint = size.height - 60;
+    path.lineTo(0, 0);
+    path.quadraticBezierTo(size.width / 4, lowPoint, size.width / 2, highPoint);
+    path.quadraticBezierTo(
+        size.width * 0.9, size.height * 0.01, size.width, size.height);
+    path.lineTo(size.width, 0);
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    return true;
   }
 }
